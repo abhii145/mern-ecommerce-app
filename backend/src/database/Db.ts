859a5313123a49348a83f76345dbce1bdd96be2e
Abhishek Yadav <55094244+abhii145@users.noreply.github.com>
2024-06-07 20:58:47 +0530

    database setup

---
+++ backend/src/database/Db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+  try {
+      await mongoose.connect(process.env.MONGODB_URI as string, {
+        dbName: "Ecommerce",
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Failed to connect to MongoDB", error);
+  }
+};
+
+export default connectToMongoDB;
