3c50b9432bb95950d3c292a1985adbb88d054ba6
Abhishek Yadav <55094244+abhii145@users.noreply.github.com>
2024-06-08 16:47:45 +0530

    product-api

--- backend/src/controllers/product.ts
+++ backend/src/controllers/product.ts
@@ -1,7 +1,103 @@
+import { invalidatesCache } from './../utils/features';
 import { BaseQuery } from "./../types/types";
 import { rm } from "fs";
 import { Product } from "./../models/product";
 import { Request, Response } from "express";
+import { myCache } from "..";
+
+
+
+export const getLatestProducts = async (req: Request, res: Response) => {
+  try {
+    let products;
+
+    if (myCache.has("latestProducts")) {
+      products = JSON.parse(myCache.get("latestProducts")!);
+    } else {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+      myCache.set("latestProducts", JSON.stringify(products));
+    }
+
+    return res
+      .status(201)
+      .json({ success: true, products, message: "Product created" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getAllProductByCategory = async (req: Request, res: Response) => {
+  try {
+    let categoriesProducts;
+
+    if (myCache.has("categoriesProducts")) {
+      categoriesProducts = JSON.parse(myCache.get("categoriesProducts")!);
+    } else {
+      categoriesProducts = await Product.find()
+        .sort({ createdAt: -1 })
+        .limit(5);
+      myCache.set("categoriesProducts", JSON.stringify(categoriesProducts));
+    }
+
+    return res.status(201).json({ success: true, categoriesProducts });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getAdminProducts = async (req: Request, res: Response) => {
+  try {
+    let products;
+    if (myCache.has("admin-Products")) {
+      products = JSON.parse(myCache.get("admin-Products")!);
+    } else {
+      products = await Product.find();
+      myCache.set("admin-Products", JSON.stringify(products));
+    }
+    return res.status(201).json({ success: true, products });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Product id is required" });
+    }
+
+    let product;
+    if (myCache.has(`singleProduct-${id}`)) {
+      product = JSON.parse(myCache.get(`singleProduct-${id}`)!);
+    } else {
+      product = await Product.findById(id);
+
+      if (!product) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Product not found" });
+      }
+      myCache.set(`singleProduct-${id}`, JSON.stringify(product));
+    }
+
+    return res.status(201).json({ success: true, product });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 export const newProduct = async (req: Request, res: Response) => {
   try {
@@ -31,75 +127,14 @@ export const newProduct = async (req: Request, res: Response) => {
       photo: photo?.path,
     });
 
+    await invalidatesCache({ product: true });
+
     return res.status(201).json({ success: true, message: "Product created" });
   } catch (error) {
     console.error(error);
     return res
       .status(500)
       .json({ success: false, message: "Internal Server Error" });
-  }
-};
-
-export const getLatestProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await Product.find().sort({ createdAt: -1 }).limit(5);
-
-    return res
-      .status(201)
-      .json({ success: true, products, message: "Product created" });
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ success: false, message: "Internal Server Error" });
-  }
-};
-
-export const getAllProductByCategory = async (req: Request, res: Response) => {
-  try {
-    const categories = await Product.distinct("category");
-    return res.status(201).json({ success: true, categories });
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ success: false, message: "Internal Server Error" });
-  }
-};
-
-export const getAdminProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await Product.find();
-
-    return res.status(201).json({ success: true, products });
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ success: false, message: "Internal Server Error" });
-  }
-};
-
-export const getSingleProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-
-    if (!id) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Product id is required" });
-    }
-
-    const product = await Product.findById(id);
-
-    if (!product) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Product not found" });
-    }
-    return res.status(201).json({ success: true, product });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
@@ -130,6 +165,8 @@ export const updateProductById = async (req: Request, res: Response) => {
     if (category) product.category = category;
 
     await product.save();
+        await invalidatesCache({ product: true });
+
 
     return res.status(201).json({ success: true, message: "Product updated" });
   } catch (error) {
@@ -147,6 +184,8 @@ export const deleteProductById = async (req: Request, res: Response) => {
     }
 
     const product = await Product.findByIdAndDelete(id);
+        await invalidatesCache({ product: true });
+
 
     if (product?.photo) {
       rm(product.photo!, () => {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,17 @@ import connectToMongoDB from "./database/Db";
 import userRouter from "./routes/user";
 import dotenv from "dotenv";
 import productRouter from "./routes/product";
+import NodeCache from "node-cache";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+
+export const myCache = new NodeCache();
+
+
 
 app.use("/api/v1/user", userRouter);
 app.use("/uploads", express.static("uploads"));
